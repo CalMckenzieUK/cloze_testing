19f7abbb6e68be864b275d7bc082ac6bab08d048
Cal Mckenzie <87912885+CalMckenzieUK@users.noreply.github.com>
2025-11-03 14:40:37 +0000

    updated app.tsx

---
+++ homepage/vite.config.ts
@@ -0,0 +1,10 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // ADD THIS LINE
+  base: '/cloze_testing/', 
+  plugins: [react()],
+})
