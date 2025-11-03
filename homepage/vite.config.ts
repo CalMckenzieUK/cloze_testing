4e7639ef89a565ba6197a66425cf82ca7a608248
Cal Mckenzie <87912885+CalMckenzieUK@users.noreply.github.com>
2025-11-03 15:02:28 +0000

    added tailwind css

--- homepage/vite.config.ts
+++ homepage/vite.config.ts
@@ -1,10 +1,11 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // IMPORT THIS
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // ADD THIS LINE
   base: '/cloze_testing/', 
-  plugins: [react()],
+  plugins: [react(), tailwindcss()]
 })
