2888001d2c1b240afb137747b0bfb702396534f9
Cal Mckenzie <87912885+CalMckenzieUK@users.noreply.github.com>
2025-11-04 15:18:45 +0000

    added tailwindcss init to deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss' // IMPORT THIS
-// import tailwindcss from '@tailwindcss/node/dist/index.mjs'
 
 export default defineConfig({
   css: {
