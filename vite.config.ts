b6392af62c37946e8ba3f2317cfc5240493b821e
Cal Mckenzie <87912885+CalMckenzieUK@users.noreply.github.com>
2025-11-04 15:15:24 +0000

    amended vite.config to remove vite-specific import

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // IMPORT THIS
+// import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss' // IMPORT THIS
 // import tailwindcss from '@tailwindcss/node/dist/index.mjs'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // ADD THIS LINE 
-  plugins: [react(), 
-    tailwindcss()
-  ],
-  base: '/cloze_testing/'
-})
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+});
