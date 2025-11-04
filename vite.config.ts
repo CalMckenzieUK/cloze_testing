6244400ddd8c350d705fb86e0faec7ecc537bb55
Cal Mckenzie <87912885+CalMckenzieUK@users.noreply.github.com>
2025-11-04 15:12:10 +0000

    removed node from gh deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // IMPORT THIS
+// import tailwindcss from '@tailwindcss/node/dist/index.mjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // ADD THIS LINE 
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), 
+    tailwindcss()
+  ],
   base: '/cloze_testing/'
 })
